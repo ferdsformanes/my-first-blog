ef15112e30674c44c86b06ebad360ab50b002f4d
ferdsformanes <ferds.formanes@gmail.com>
2025-03-04 00:46:34 +0800

    Add security and comment model to blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,9 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGOUT_REDIRECT_URL = '/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
